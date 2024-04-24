7239140a7f1e6d3a5c8d6f19b88b33086f844744
Esteban Luna S_Linux <lunasena.esteban@gmail.com>
2024-04-23 22:03:22 -0500

    Se realiza repaso de conceptos desde linux

--- src/RepasoEyS/RepasoEyS.java
+++ src/RepasoEyS/RepasoEyS.java
@@ -10,34 +10,35 @@ public class RepasoEyS {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		suma suma = new suma();
+		Suma suma = new Suma();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		System.out.println("Hola, probemos usar el método sumar de sde suma.java: \n");
+		System.out.printf("***Haciendo uso de la clase Suma*** \n");
 
-		System.out.println("Ingrese un número 1: ");
+		System.out.printf("Ingrese un número 1: ");
 		int x1 = Integer.parseInt(br.readLine());
 		
-		System.out.println("Ingrese un número 2: ");
+		System.out.printf("Ingrese un número 2: ");
 		int x2 = Integer.parseInt(br.readLine());
 		
-		System.out.println("Suma= " + suma.sumar(x1,x2));	
-		System.out.println("La suma es: " + (x1+x2));
+		System.out.printf("\nSuma= %d\n", + suma.sumar(x1,x2));
+		System.out.printf("La suma es: %d\n", + (x1+x2));
 
-	
+		//Se llama al método saludos desde suma
 		suma.saludo();
 
 
-		int i=1, v1 = 1; int v2 = 45; int random=0;
-		System.out.printf("\n");
+		int i=1, v1 = 1, v2 = 100, random=0, acum=0;
 		do {
 			random = (int) ((Math.random() * v2));
 			if (random>v1 && random<v2) {
 				System.out.println(random);
 				i++;
 			}
-		}while (i<=5);
+			acum++;
+		}while (i<=10);
+		System.out.printf("\n Se realizaron %d iteraciones", acum);
 
 	}
 	}
